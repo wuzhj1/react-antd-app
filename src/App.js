2a88b628d517d26ca0944ae79eb9e304df6d6c09
wuzhj1 <1970097504@qq.com>
2019-11-12 17:00:43 +0800

    asdfa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component}from 'react';
+import { Button } from 'antd'
+class App extends Component{
+  
+  render(){
+    return (
+    <div >
+      <Button type='primary'> asdfasd</Button>
+    </div>
+    )};
+   
+  
+}
+
+export default App;
